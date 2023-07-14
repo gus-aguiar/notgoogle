2e8c8d75c6f438aab1bab578999515dcfff26690
gus-aguiar <gustavopma90@gmail.com>
2023-07-14 20:36:51 -0300

    final commit day 1

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,18 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    linhas = []
+    for i in range(len(instance)):
+        for j in range(len(instance.search(i)["linhas_do_arquivo"])):
+            if (
+                word.lower()
+                in instance.search(i)["linhas_do_arquivo"][j].lower()
+            ):
+                linhas.append({"linha": j + 1})
+    dict = {
+        "palavra": word,
+        "arquivo": instance.search(i)["nome_do_arquivo"],
+        "ocorrencias": linhas,
+    }
+    return [dict] if len(linhas) > 0 else []
 
 
 def search_by_word(word, instance):
