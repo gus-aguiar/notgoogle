6851785970a2fb8fa947fb331f58c7e9ae28655e
gus-aguiar <gustavopma90@gmail.com>
2023-07-14 16:12:13 -0300

    reqs 1 e 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
+import os
+import sys
+
+
 def txt_importer(path_file):
-    """initial commit"""
+    if not str(path_file).endswith(".txt"):
+        return sys.stderr.write("Formato inválido")
+
+    elif not os.path.exists(path_file):
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+
+    else:
+        f = open(path_file, "r")
+        data = f.read().split("\n")
+        f.close()
+        return data
